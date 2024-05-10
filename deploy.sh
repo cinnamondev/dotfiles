3e9769e60d761f24236ee3369fb525dba31cfc19
cinnamondev <justpromptiguess@gmail.com>
2024-05-10 19:25:32 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+git clone --separate-git-dir=$HOME/.dotfiles -b "${1}" git@github.com:/cinnamondev/dotfiles.git ~/.dotfiles-tmp
+
+rsync --recursive --verbose --exclude ".git*" --exclude "README.md" ~/.dotfiles-tmp/ ~/testSync
+
+rm -r ~/.dotfiles-tmp
