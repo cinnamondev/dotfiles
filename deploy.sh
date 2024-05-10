8644926c20b140ac8ccbaffb0fd626189de1b578
cinnamondev <justpromptiguess@gmail.com>
2024-05-10 20:02:49 +0100

    script upd

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
 git clone --separate-git-dir=$HOME/.dotfiles -b "${1}" git@github.com:/cinnamondev/dotfiles.git ~/.dotfiles-tmp
 
-rsync --recursive --verbose --exclude ".git*" --exclude "README.md" ~/.dotfiles-tmp/ ~/testSync
+rsync --recursive --verbose --exclude ".git*" --exclude "README.md" ~/.dotfiles-tmp/ $HOME
 
 rm -r ~/.dotfiles-tmp
